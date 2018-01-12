dff42850c0ab8a30ef799f8a16e933f4617fc04b
Daniel Limberger <cgdaniel@outlook.com>
2018-01-12 14:06:32 +0100

    refine travis-ci config and build-all script

--- webpack.config.all.js
+++ webpack.config.all.js
@@ -5,6 +5,7 @@ var webpack = require('webpack');
 var createExportObject = require('./webpack.export.js');
 
 module.exports = [
+  createExportObject('webgl-operate', false),
   createExportObject('webgl-operate', true),
   createExportObject('webgl-operate.slim', false),
   createExportObject('webgl-operate.slim', true)];
