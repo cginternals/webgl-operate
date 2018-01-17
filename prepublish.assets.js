37ee58c041e555c9bc5378c280ad366d90120032
Daniel Limberger <cgdaniel@outlook.com>
2018-01-17 15:15:54 +0100

    resolve duplicate

--- prepublish.assets.js
+++ prepublish.assets.js
@@ -3,40 +3,5 @@
  * for publishing this module as npm package. For distribution, webpack bundles all required assets.
  */
 
-// const watch = process.argv.indexOf('--watch') > 1;
-
-const fs = require('fs');
-const glob = require("glob")
-const path = require('path');
-
-const baseDir = './source';
-const distDir = './lib';
-
-const assets = ['**/*.{json,glsl,frag,vert}'];
-
-function build() {
-
-    assets.forEach(asset => {
-
-        glob(asset, { cwd: baseDir, strict: true }, function (error, files) {
-            files.forEach(file => {
-                const src = path.join(baseDir, file);
-                const dst = path.join(distDir, file);
-                if (!fs.existsSync(path.dirname(dst))) {
-                    fs.mkdirSync(path.dirname(dst));
-                }
-                fs.createReadStream(src).pipe(fs.createWriteStream(dst));
-                console.log('emitted:', dst);
-            });
-        })
-    });
-
-}
-
-// if (watch) {
-//     const watcher = fs.watch(baseDir, { recursive: true }, build());
-//     watcher.close();
-// } else {
-build();
-// }
-
+const copy = require('./copy.js');
+copy('./source', './lib', ['**/*.{json,glsl,frag,vert}']);
--- pugconfig.js
+++ pugconfig.js
@@ -17,40 +17,18 @@ const distDir = './dist';
 const assets = ['css/*.css', 'js/*.js', 'img/*.{svg,png}'];
 const entries = ['index.pug'];
 
+const copy = require('./copy.js');
+copy(baseDir, distDir, ['css/*.css', 'js/*.js', 'img/*.{svg,png}']);
 
-function build() {
-
-    assets.forEach(asset => {
-        glob(asset, { cwd: baseDir, strict: true }, function (error, files) {
-            files.forEach(file => {
-                const src = path.join(baseDir, file);
-                const dst = path.join(distDir, file);
-                if (!fs.existsSync(path.dirname(dst))) {
-                    fs.mkdirSync(path.dirname(dst));
-                }
-                fs.createReadStream(src).pipe(fs.createWriteStream(dst));
-                console.log('emitted:', dst);
-            });
-        });
-    });
-
-    entries.forEach((entry) => {
-        const src = path.join(baseDir, entry);
-        const dst = path.join(distDir, path.basename(entry, path.extname(entry)) + '.html');
-        if (!fs.existsSync(src)) {
-            console.log('skipped:', entry);
-            return;
-        }
-        const html = pug.renderFile(src);
-        fs.writeFileSync(dst, html);
-        console.log('emitted:', dst);
-    });
-}
-
-// if (watch) {
-//     const watcher = fs.watch(baseDir, { recursive: true }, build());
-//     watcher.close();
-// } else {
-build();
-// }
+entries.forEach((entry) => {
+    const src = path.join(baseDir, entry);
+    const dst = path.join(distDir, path.basename(entry, path.extname(entry)) + '.html');
+    if (!fs.existsSync(src)) {
+        console.log('skipped:', entry);
+        return;
+    }
+    const html = pug.renderFile(src);
+    fs.writeFileSync(dst, html);
+    console.log('emitted:', dst);
+});
 
