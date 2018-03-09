78aa5dd242abca04194600e1cd4c93429e0fdb22
Daniel Limberger <cgdaniel@outlook.com>
2018-03-09 14:43:50 +0100

    refine wizard for texture format query

--- source/wizard.ts
+++ source/wizard.ts
@@ -25,8 +25,6 @@ export class Wizard {
         const gl = context.gl;
         const gl2facade = context.gl2facade;
 
-        assert(target === gl.RGBA, `internal format querying is not yet supported for formats other than RGBA`);
-
         /**
          * In WebGL1 contexts, OES_texture_float always implies WEBGL_color_buffer_float and OES_texture_half_float
          * implies EXT_color_buffer_half_float.
@@ -41,21 +39,40 @@ export class Wizard {
             query = floatWriteSupport ? 'float' : halfWriteSupport ? 'half' : 'byte';
         }
 
-        /* If required, enable extension. */
+        let type: GLenum;
+        let internalFormatIndex: GLint; /* Utility index to reduce tuple return logic (see switch). */
+
+        /* Query type and, if required), enable extension. */
         if (query === 'half' && halfWriteSupport) {
             /* tslint:disable-next-line:no-unused-expression */
             context.isWebGL2 ? context.colorBufferFloat : context.textureHalfFloat;
-            return [context.isWebGL2 ? gl.RGBA16F : gl.RGBA, gl2facade.HALF_FLOAT];
-
+            type = gl2facade.HALF_FLOAT;
+            internalFormatIndex = 1;
         } else if ((query === 'float' || query === 'half') && floatWriteSupport) {
             /* If not explicitly requested, fallback for half_float. */
             /* tslint:disable-next-line:no-unused-expression */
             context.isWebGL2 ? context.colorBufferFloat : context.textureFloat;
-            return [context.isWebGL2 ? gl.RGBA32F : gl.RGBA, gl.FLOAT];
-
+            type = gl.FLOAT;
+            internalFormatIndex = 0;
+        } else {
+            type = gl.UNSIGNED_BYTE;
+            internalFormatIndex = 2;
         }
-        /* If not explicitly requested, fallback for float. */
-        return [context.isWebGL2 ? gl.RGBA8 : gl.RGBA, gl.UNSIGNED_BYTE];
+
+        /* In this case, no specialized internal formats are available. */
+        if (context.isWebGL1) {
+            return [target, type];
+        }
+
+        switch (target) {
+            case gl.RGBA:
+                return [[gl.RGBA32F, gl.RGBA16F, gl.RGBA8][internalFormatIndex], type];
+            case gl.RGB:
+                return [[gl.RGB32F, gl.RGB16F, gl.RGB8][internalFormatIndex], type];
+            default:
+                assert(false, `internal format querying is not yet supported for formats other than RGBA, RGB`);
+        }
+        return [gl.NONE, gl.NONE];
     }
 
 }
