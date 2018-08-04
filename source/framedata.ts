7eda1e60103be91731c51d5f1ad2077d8db3f828
Benjamin Wasty <benny.wasty@gmail.com>
2018-08-04 10:50:07 +0200

    finish basics of FrameData

--- source/framedata.ts
+++ source/framedata.ts
@@ -1,22 +1,37 @@
 import { RenderView } from './renderview';
-import { XRFrame, XRFrameOfReference, XRInputSource, XRInputPose } from './webxr';
+import { XRFrame, XRFrameOfReference, XRInputPose, XRInputSource } from './webxr';
+
+interface XRInputData {
+    source: XRInputSource;
+    /** When tracking is lost, the pose might be `null` */
+    pose: XRInputPose | null;
+}
 
 /**
  */
 export class FrameData {
+    private _time: number;
     private _frame: XRFrame;
+    private _frameOfReference: XRFrameOfReference;
+    private _renderViews: RenderView[] = [];
+    private _inputData: XRInputData[] = [];
+
+    /** The timestamp passed to requestAnimationFrame */
+    get time(): number { return this._time; }
+    set time(time: number) {
+        this._time = time;
+    }
+
     get frame(): XRFrame { return this._frame; }
     set frame(frame: XRFrame) {
         this._frame = frame;
-        // TODO!!: invalidate renderviews...?
     }
 
-    private _frameOfReference: XRFrameOfReference;
     get frameOfReference(): XRFrameOfReference { return this._frameOfReference; }
-    set frameOfReference(frame: XRFrameOfReference) {
+    set frameOfReference(frameOfReference: XRFrameOfReference) {
+        this._frameOfReference = frameOfReference;
     }
 
-    private _renderViews: RenderView[] = [];
     get renderViews(): RenderView[] {
         const pose = this.frame.getDevicePose(this.frameOfReference);
         // Getting the pose may fail if, for example, tracking is lost.
@@ -39,35 +54,27 @@ export class FrameData {
                     this.frame.session.baseLayer.getViewport(view)!,
                 );
             }
-        } // else: return last frame's data (assumption: instance is reused)
+        } // else: return last frame's data (assumption: object is reused)
 
         return this._renderViews;
     }
 
-    /**
-     * XRInputSources are generally 3DOF/6DOF hand controllers
-     * 3DOF controllers often have emulated 3D positions - signified
-     * by the `emulatedPosition` field
-     */
-    get inputSources(): XRInputSource[] {
-        return this.frame.session.getInputSources();
-    }
-
-    private _inputPoses: Array<XRInputPose | null> = [];
-
-    /**
-     *
-     */
-    get inputPoses(): Array<XRInputPose | null>[] {
-        // TODO!!
-        const inputSources = this.inputSources;
-
-        // Re-using the same array to avoid allocations per frame -> adjust length in case
-        // number of input sources changes
-        if (inputSources.length !== this.inputPoses.length) {
-            this.inputPoses.length = inputSources.length;
+    get inputData(): XRInputData[] {
+        const sources = this._frame.session.getInputSources();
+        if (sources.length !== this.inputData.length) {
+            this.inputData.length = sources.length;
         }
 
-        return
+        for (let i = 0; i < sources.length; ++i) {
+            const source = sources[i];
+            const pose = this._frame.getInputPose(sources[i], this.frameOfReference);
+            if (!this.inputData[i]) {
+                this.inputData[i] = {} as XRInputData;
+            }
+            this.inputData[i].source = source;
+            this.inputData[i].pose = pose;
+        }
+
+        return this._inputData;
     }
 }
