9568e0a59a5990cad8812b5e0c338c826e1028b1
Daniel Limberger <cgdaniel@outlook.com>
2018-10-09 14:25:10 +0200

    Update pugconfig.js

--- pugconfig.js
+++ pugconfig.js
@@ -20,10 +20,11 @@ const entries = ['index.pug'];
 const assets = [
     [websiteDir, buildDir, ['css/*.css', 'js/*.js', 'img/*.{svg,png}', 'fonts/*', '*.{svg,png,ico,xml,json}'], [], false],
     ['./source', buildDir, ['data/{colorbrewer,smithwalt}.json'], [], false],
+    ['./node_modules/rxjs/bundles/', `${buildDir}/js`, ['rxjs.umd.min.js'], [], false],
     ['./source/data/', `${buildDir}/data`, ['*'], [], false],
     ['./source/data/opensansr144', `${buildDir}/data/opensansr144`, ['*'], [], false],
     ['./source/data/verdana', `${buildDir}/data/verdana`, ['*'], [], false],
-    ['./node_modules/rxjs/bundles/', `${buildDir}/js`, ['rxjs.umd.min.js'], [], false]];
+];
 
 
 var build_pending = false;
