2b9d176e6c280c3a6bf3115a099eafb5bf9617bd
Daniel Limberger <cgdaniel@outlook.com>
2018-11-19 02:02:22 +0100

    fix ie11

--- source/polyfill.ts
+++ source/polyfill.ts
@@ -84,6 +84,51 @@ if (Array.prototype.forEach === undefined) {
 }
 
 /**
+ * IE11 polyfill for Array.fill function, from ...
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/fill
+ */
+if (Array.prototype.fill === undefined) {
+    Array.prototype.fill = (value, start: number, end: number) => {
+
+        // Steps 1-2.
+        if (this === null) {
+            throw new TypeError('this is null or not defined');
+        }
+
+        const O = Object(this);
+
+        // Steps 3-5.
+        const len = O.length >>> 0;
+
+        // Steps 6-7.
+        const relativeStart = start >> 0;
+
+        // Step 8.
+        let k = relativeStart < 0 ?
+            Math.max(len + relativeStart, 0) :
+            Math.min(relativeStart, len);
+
+        // Steps 9-10.
+        const relativeEnd = end === undefined ?
+            len : end >> 0;
+
+        // Step 11.
+        const final = relativeEnd < 0 ?
+            Math.max(len + relativeEnd, 0) :
+            Math.min(relativeEnd, len);
+
+        // Step 12.
+        while (k < final) {
+            O[k] = value;
+            ++k;
+        }
+
+        // Step 13.
+        return O;
+    };
+}
+
+/**
  * IE11 polyfill for Math.log10 function
  */
 if (Math.log10 === undefined) {
