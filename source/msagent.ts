0bd19615d4953552d177b9f871e8c3081bd8fdce
cgcostume <cgdaniel@outlook.com>
2019-03-07 14:06:48 +0100

    #158 prevent default drag action in edge

---
+++ source/msagent.ts
@@ -0,0 +1,5 @@
+
+export const IS_IE11 = typeof navigator === 'undefined' ?
+    undefined : /MSIE 11/i.test(navigator.userAgent);
+export const IS_EDGE = typeof navigator === 'undefined' ?
+    undefined : /Edge\/\d./i.test(navigator.userAgent);
