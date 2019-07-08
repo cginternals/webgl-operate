95251bab498792bf061a7e4cb6e7427e0f5f2e0d
Philipp Otto <philipp.otto2@student.hpi.de>
2019-07-08 15:57:29 +0200

    Move shaders out of environment mapping example, fix some linter issues

--- examples/envprojections-example.ts
+++ examples/envprojections-example.ts
@@ -5,20 +5,20 @@ import { auxiliaries } from 'webgl-operate';
 
 import {
     Camera,
-    Context,
     Canvas,
+    Context,
     DefaultFramebuffer,
     Framebuffer,
+    Invalidate,
+    MouseEventProvider,
     Navigation,
     NdcFillingTriangle,
     Program,
     Renderer,
     Shader,
-    MouseEventProvider,
     Texture2D,
     TextureCube,
     Wizard,
-    Invalidate,
 } from 'webgl-operate';
 
 import { Example } from './example';
@@ -26,122 +26,6 @@ import { Example } from './example';
 // tslint:disable:max-classes-per-file
 
 class EnvironmentProjectionRenderer extends Renderer {
-
-    protected SHADER_SOURCE_VERT =
-        `
-        precision highp float;
-
-        #if __VERSION__ == 100
-            attribute vec2 a_vertex;
-        #else
-            in vec2 a_vertex;
-            #define varying out
-        #endif
-
-        uniform mat4 u_viewProjectionInverse;
-
-        varying vec2 v_uv;
-        varying vec4 v_ray;
-
-        void main(void)
-        {
-            v_uv = a_vertex * 0.5 + 0.5;
-            v_ray = u_viewProjectionInverse * vec4(a_vertex, 1.0, 1.0);
-
-            gl_Position = vec4(a_vertex.xy, 1.0, 1.0);
-        }
-        `;
-
-    protected SHADER_SOURCE_FRAG =
-        `
-        precision highp float;
-        precision highp int;
-
-        #if __VERSION__ == 100
-            #define fragColor gl_FragColor
-        #else
-            layout(location = 0) out vec4 fragColor;
-            #define varying in
-        #endif
-
-        uniform ivec2 u_viewport;
-        uniform float u_time;
-
-        uniform int u_mode;
-
-        uniform samplerCube u_cubemap;
-        uniform sampler2D u_equirectmap;
-        uniform sampler2D u_spheremap;
-        uniform sampler2D u_polarmap[2];
-
-        varying vec2 v_uv;
-        varying vec4 v_ray;
-
-        const float aspect = 1.0 / 1.0;
-
-        const float PI = 3.141592653589793;
-        const float OneOver2PI = 0.1591549430918953357688837633725;
-        const float OneOverPI  = 0.3183098861837906715377675267450;
-
-        void main(void)
-        {
-            vec2 uv = v_uv;
-
-            // fragColor = vec4(vec3(uv.s, uv.t, float(u_mode) * 0.25), 1.0);
-            fragColor = vec4(v_ray.xyz * 0.5 + 0.5, 1.0);
-
-            // fragColor = vec4(uv, 0.0, 1.0);
-            vec3 ray = normalize(v_ray.xyz);
-
-            // vec3 dx = dFdx(ray);
-            // vec3 dy = dFdy(ray);
-
-            // fragColor = texture(u_cubemap, ray);
-
-            // // fragColor = vec4(vec3((abs(dx.x) + abs(dx.y) + abs(dx.z)
-            // //     + abs(dy.x) + abs(dy.y) + abs(dy.z)) * 100.0), 1.0);
-            // return;
-
-            if(u_mode == 0) {
-
-                fragColor = texture(u_cubemap, vec3(ray));
-
-            } else if (u_mode == 1) {
-
-                float v = acos(ray.y) * OneOverPI;
-                //ray.zx = ray.xz;
-                //ray.xz = ray.zx;
-                //ray.z *= -1.0;
-                //ray.x *= -1.0;
-
-                float m = atan(ray.x, ray.z);
-                uv = vec2(m * OneOver2PI + 0.5, v);
-
-                fragColor = texture(u_equirectmap, uv);
-
-            } else if (u_mode == 2) {
-                // ray.y *= -1.0;
-                ray = ray.xzy;
-                ray.z += +1.0;
-                uv = 0.5 + 0.5 * ray.xy / length(ray);
-
-                fragColor = texture(u_spheremap, uv);
-
-            } else if (u_mode == 3) {
-
-                //ray = ray.zyx;
-                ray.y *= -1.0;
-
-                float m = 1.0 + abs(asin(ray.y) * 2.0 / PI);
-                uv = 0.5 + 0.5 * ray.xz / m;
-
-                fragColor = mix(texture(u_polarmap[0], uv),
-                                texture(u_polarmap[1], vec2(1.0, -1.0) * uv),
-                                step(0.0, ray.y));
-
-            }
-        }
-    `;
 
     protected _defaultFBO: Framebuffer;
 
@@ -184,11 +68,11 @@ class EnvironmentProjectionRenderer extends Renderer {
         this.fetchTextures();
 
         // Initialize program and uniforms
-        const vert = new Shader(this._context, gl.VERTEX_SHADER, 'ndcvertices (in-line)');
-        vert.initialize(this.SHADER_SOURCE_VERT);
+        const vert = new Shader(this._context, gl.VERTEX_SHADER, 'ndcvertices');
+        vert.initialize(require('./data/env_mapping.vert'));
 
-        const frag = new Shader(this._context, gl.FRAGMENT_SHADER, 'env-projections (in-line)');
-        frag.initialize(this.SHADER_SOURCE_FRAG);
+        const frag = new Shader(this._context, gl.FRAGMENT_SHADER, 'env-projections');
+        frag.initialize(require('./data/env_mapping.frag'));
 
         this._program = new Program(this._context, 'EnvProjectionsProgram');
         this._program.initialize([vert, frag], false);
@@ -210,7 +94,7 @@ class EnvironmentProjectionRenderer extends Renderer {
 
         // Initialize camera
         this._camera = new Camera();
-        this._camera.eye = vec3.fromValues(0.0, +0.5, +1.0);
+        this._camera.eye = vec3.fromValues(0.0, 0.5, 1.0);
         this._camera.center = vec3.fromValues(0.0, 0.0, 0.0);
         this._camera.up = vec3.fromValues(0.0, 1.0, 0.0);
         this._camera.near = 0.1;
@@ -265,7 +149,7 @@ class EnvironmentProjectionRenderer extends Renderer {
         gl.uniformMatrix4fv(this._uViewProjection, gl.GL_FALSE, this._camera.viewProjection);
         gl.uniformMatrix4fv(this._uViewProjectionInverse, gl.GL_FALSE, this._camera.viewProjectionInverse);
 
-        let t = ((new Date()).getTime() % 10000000) * 0.001;
+        const t = ((new Date()).getTime() % 10000000) * 0.001;
         gl.uniform1f(this._uTime, t);
 
         this._ndcTriangle.bind();
@@ -294,7 +178,6 @@ class EnvironmentProjectionRenderer extends Renderer {
 
         gl.disable(gl.SCISSOR_TEST);
 
-
         this._ndcTriangle.unbind();
 
         this._program.unbind();
@@ -320,7 +203,6 @@ class EnvironmentProjectionRenderer extends Renderer {
 
         const internalFormatAndType = Wizard.queryInternalTextureFormat(
             this._context, gl.RGB, Wizard.Precision.byte);
-
 
         this._cubeMap = new TextureCube(this._context);
         this._cubeMap.initialize(512, internalFormatAndType[0], gl.RGB, internalFormatAndType[1]);
