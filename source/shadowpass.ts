be735981ee34557a30d81665885646c766599b23
Jan Ole Vollmer <jan.vollmer@hpi.de>
2019-09-10 16:40:50 +0200

    Fix uninitialization of blurTexture

--- source/shadowpass.ts
+++ source/shadowpass.ts
@@ -190,7 +190,7 @@ export class ShadowPass extends Initializable {
             this._intermediateBlurTexture.uninitialize();
 
             this._blurFBO.uninitialize();
-            this._intermediateBlurTexture.uninitialize();
+            this._blurTexture.uninitialize();
 
             this._gaussFilter.uninitialize();
         }
