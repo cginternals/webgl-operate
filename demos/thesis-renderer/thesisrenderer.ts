24b1f391289aca21491125773a52ce701f1d73f3
Philipp Otto <philipp.otto2@student.hpi.de>
2020-02-04 14:14:58 +0100

    Add support for touch events for navigation

--- demos/thesis-renderer/thesisrenderer.ts
+++ demos/thesis-renderer/thesisrenderer.ts
@@ -31,6 +31,7 @@ import {
     ShadowPass,
     Texture2D,
     TextureCube,
+    TouchEventProvider,
     Wizard,
 } from 'webgl-operate';
 
@@ -48,6 +49,8 @@ import { DiffuseEnvironmentSample, LightSample, SampleManager, SpecularEnvironme
  * @todo comment
  */
 export class ThesisRenderer extends Renderer {
+
+    static URL = 'https://p-otto.waduhek.de';
 
     protected _loader: GLTFLoader;
     protected _loading: boolean;
@@ -170,7 +173,7 @@ export class ThesisRenderer extends Renderer {
     protected onInitialize(context: Context, callback: Invalidate,
         mouseEventProvider: MouseEventProvider,
         /* keyEventProvider: KeyEventProvider, */
-        /* touchEventProvider: TouchEventProvider */): boolean {
+        touchEventProvider: TouchEventProvider): boolean {
 
         const gl = this._context.gl;
         const gl2facade = this._context.gl2facade;
@@ -180,7 +183,7 @@ export class ThesisRenderer extends Renderer {
 
         this._loader = new GLTFLoader(this._context);
 
-        this._cornellScene = new Scene('https://p-otto.waduhek.de/models/cornell.glb',
+        this._cornellScene = new Scene(`${ThesisRenderer.URL}/models/cornell.glb`,
             new Camera(vec3.fromValues(-0.255, 3.09, -8.0), vec3.fromValues(0.135, 1.192, -0.46)),
             0.2, 20);
         this._cornellScene.addDiskLight(new DiskLight(
@@ -191,7 +194,7 @@ export class ThesisRenderer extends Renderer {
             90.0));
 
         this._skylineScene = new Scene(
-            'https://p-otto.waduhek.de/models/skyline.glb',
+            `${ThesisRenderer.URL}/models/skyline.glb`,
             new Camera(vec3.fromValues(-4.0645, 2.816, 6.2326), vec3.fromValues(0.342, -0.42328, 0.1032)),
             // new Camera(vec3.fromValues(1.7162, 0.6412, 4.2596), vec3.fromValues(-0.7943, -0.17933, -0.7432)),
             0.2, 20);
@@ -221,7 +224,7 @@ export class ThesisRenderer extends Renderer {
             110.0));
 
         this._datsunScene = new Scene(
-            'https://p-otto.waduhek.de/models/datsun.glb',
+            `${ThesisRenderer.URL}/models/datsun.glb`,
             new Camera(vec3.fromValues(-1.9631, 1.89, 6.548), vec3.fromValues(0.292, -0.327, -0.13)),
             0.2, 30);
         this._datsunScene.addDiskLight(new DiskLight(
@@ -250,7 +253,7 @@ export class ThesisRenderer extends Renderer {
             110.0));
 
         this._kitchenScene = new Scene(
-            'https://p-otto.waduhek.de/models/kitchen.glb',
+            `${ThesisRenderer.URL}/models/kitchen.glb`,
             new Camera(vec3.fromValues(-0.65597, 2.2284, 6.2853), vec3.fromValues(0.24971, 1.1144, -0.7265)),
             // new Camera(vec3.fromValues(-3.479, 1.604, -0.5713), vec3.fromValues(0.0, 1.0673, -0.8168)),
             0.1, 10);
@@ -398,7 +401,7 @@ export class ThesisRenderer extends Renderer {
 
         /* Create and configure navigation */
 
-        this._navigation = new Navigation(callback, mouseEventProvider);
+        this._navigation = new Navigation(callback, mouseEventProvider, touchEventProvider);
         this._navigation.camera = this._camera;
 
         /**
@@ -1020,12 +1023,12 @@ export class ThesisRenderer extends Renderer {
         this._diffuseEnvironment.initialize(64, internalFormatAndType[0], gl.RGBA, internalFormatAndType[1]);
 
         this._diffuseEnvironment.fetch({
-            positiveX: `https://p-otto.waduhek.de/${environmentName}/preprocessed-map-px-diffuse.png`,
-            negativeX: `https://p-otto.waduhek.de/${environmentName}/preprocessed-map-nx-diffuse.png`,
-            positiveY: `https://p-otto.waduhek.de/${environmentName}/preprocessed-map-py-diffuse.png`,
-            negativeY: `https://p-otto.waduhek.de/${environmentName}/preprocessed-map-ny-diffuse.png`,
-            positiveZ: `https://p-otto.waduhek.de/${environmentName}/preprocessed-map-pz-diffuse.png`,
-            negativeZ: `https://p-otto.waduhek.de/${environmentName}/preprocessed-map-nz-diffuse.png`,
+            positiveX: `${ThesisRenderer.URL}/${environmentName}/preprocessed-map-px-diffuse.png`,
+            negativeX: `${ThesisRenderer.URL}/${environmentName}/preprocessed-map-nx-diffuse.png`,
+            positiveY: `${ThesisRenderer.URL}/${environmentName}/preprocessed-map-py-diffuse.png`,
+            negativeY: `${ThesisRenderer.URL}/${environmentName}/preprocessed-map-ny-diffuse.png`,
+            positiveZ: `${ThesisRenderer.URL}/${environmentName}/preprocessed-map-pz-diffuse.png`,
+            negativeZ: `${ThesisRenderer.URL}/${environmentName}/preprocessed-map-nz-diffuse.png`,
         });
 
         this._specularEnvironment = new TextureCube(this._context, 'SpecularEnvironment');
@@ -1038,12 +1041,12 @@ export class ThesisRenderer extends Renderer {
 
         for (let mipLevel = 0; mipLevel < MIPMAP_LEVELS; ++mipLevel) {
             this._specularEnvironment.fetch({
-                positiveX: `https://p-otto.waduhek.de/${environmentName}/preprocessed-map-px-${mipLevel}.png`,
-                negativeX: `https://p-otto.waduhek.de/${environmentName}/preprocessed-map-nx-${mipLevel}.png`,
-                positiveY: `https://p-otto.waduhek.de/${environmentName}/preprocessed-map-py-${mipLevel}.png`,
-                negativeY: `https://p-otto.waduhek.de/${environmentName}/preprocessed-map-ny-${mipLevel}.png`,
-                positiveZ: `https://p-otto.waduhek.de/${environmentName}/preprocessed-map-pz-${mipLevel}.png`,
-                negativeZ: `https://p-otto.waduhek.de/${environmentName}/preprocessed-map-nz-${mipLevel}.png`,
+                positiveX: `${ThesisRenderer.URL}/${environmentName}/preprocessed-map-px-${mipLevel}.png`,
+                negativeX: `${ThesisRenderer.URL}/${environmentName}/preprocessed-map-nx-${mipLevel}.png`,
+                positiveY: `${ThesisRenderer.URL}/${environmentName}/preprocessed-map-py-${mipLevel}.png`,
+                negativeY: `${ThesisRenderer.URL}/${environmentName}/preprocessed-map-ny-${mipLevel}.png`,
+                positiveZ: `${ThesisRenderer.URL}/${environmentName}/preprocessed-map-pz-${mipLevel}.png`,
+                negativeZ: `${ThesisRenderer.URL}/${environmentName}/preprocessed-map-nz-${mipLevel}.png`,
             }, mipLevel);
         }
     }
