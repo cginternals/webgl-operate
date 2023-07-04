a61063b723b8960513ccb9342e8a215cad9639f2
Willy Scheibel <willy.scheibel@hpi.de>
2023-07-04 11:58:11 +0200

    Intermediate commit

--- examples/ambient-occlusion-example.ts
+++ examples/ambient-occlusion-example.ts
@@ -54,10 +54,11 @@ export class AmbientOcclusionRenderer extends Renderer {
 
     protected _sceneProgram: Program;
 
-    protected _uViewProjection: WebGLUniformLocation;
-    protected _uModel: WebGLUniformLocation;
+    protected _uViewProjectionLocation: WebGLUniformLocation;
+    protected _uModelLocation: WebGLUniformLocation;
     protected _uNormalLocation: WebGLUniformLocation;
-    protected _uEye: WebGLUniformLocation;
+    protected _uSceneFrameSizeLocation: WebGLUniformLocation;
+    protected _uEyeLocation: WebGLUniformLocation;
     /* // Scene Pass */
 
     protected _sceneFbo: Framebuffer;
@@ -67,6 +68,8 @@ export class AmbientOcclusionRenderer extends Renderer {
 
     protected _aoProgram: Program;
 
+    protected _uAOFrameSizeLocation: WebGLUniformLocation;
+    protected _uNormalTextureLocation: WebGLUniformLocation;
     protected _uDepthTextureLocation: WebGLUniformLocation;
     /* // Ambient Occlusion Pass */
 
@@ -77,8 +80,9 @@ export class AmbientOcclusionRenderer extends Renderer {
 
     protected _blurProgram: Program;
 
-    protected _uKernelSize: WebGLUniformLocation;
-    protected _uaoMapLocation: WebGLUniformLocation;
+    protected _uKernelSizeLocation: WebGLUniformLocation;
+    protected _uSourceLocation: WebGLUniformLocation;
+    protected _uBlurFrameSizeLocation: WebGLUniformLocation;
     /* // Blur Pass */
 
     protected _blurredAOFbo: Framebuffer;
@@ -89,6 +93,7 @@ export class AmbientOcclusionRenderer extends Renderer {
     protected _uColorLocation: WebGLUniformLocation;
     protected _uDepthLocation: WebGLUniformLocation;
     protected _uAOMapLocation: WebGLUniformLocation;
+    protected _uCompositionFrameSizeLocation: WebGLUniformLocation;
     /* // Composition Pass */
 
     protected _outputFbo: DefaultFramebuffer;
@@ -112,6 +117,7 @@ export class AmbientOcclusionRenderer extends Renderer {
         /* Initialize Shared Geometry for Postprocessing Passes */
 
         this._ndcGeometry = new NdcFillingTriangle(this._context);
+        this._ndcGeometry.initialize();
 
         /* Create and configure camera. */
 
@@ -178,21 +184,72 @@ export class AmbientOcclusionRenderer extends Renderer {
 
         /* Initialize Programs */
 
-        const vert = new Shader(context, gl.VERTEX_SHADER, 'geometry.vert');
-        vert.initialize(require('./data/ssao/geometry.vert'));
-        const frag = new Shader(context, gl.FRAGMENT_SHADER, 'geometry.frag');
-        frag.initialize(require('./data/ssao/geometry.frag'));
+        /* Scene Pass */
 
-        this._sceneProgram = new Program(context, 'ShadingProgram');
-        this._sceneProgram.initialize([vert, frag], true);
+        const scene_vert = new Shader(context, gl.VERTEX_SHADER, 'geometry.vert');
+        scene_vert.initialize(require('./data/ssao/geometry.vert'));
+        const scene_frag = new Shader(context, gl.FRAGMENT_SHADER, 'geometry.frag');
+        scene_frag.initialize(require('./data/ssao/geometry.frag'));
+
+        this._sceneProgram = new Program(context, 'SceneProgram');
+        this._sceneProgram.initialize([scene_vert, scene_frag], true);
         this._sceneProgram.link();
         this._sceneProgram.bind();
 
-        this._uViewProjection = this._sceneProgram.uniform('u_viewProjection');
-        this._uModel = this._sceneProgram.uniform('u_model');
-        this._uEye = this._sceneProgram.uniform('u_eye');
+        this._uViewProjectionLocation = this._sceneProgram.uniform('u_viewProjection');
+        this._uModelLocation = this._sceneProgram.uniform('u_model');
+        this._uEyeLocation = this._sceneProgram.uniform('u_eye');
 
         this._uNormalLocation = this._sceneProgram.uniform('u_normal');
+        this._uSceneFrameSizeLocation = this._sceneProgram.uniform('u_frameSize');
+
+        /* Shared Programs */
+
+        const ndc_triangle_vert = new Shader(context, gl.VERTEX_SHADER, 'ndctriangle.vert');
+        ndc_triangle_vert.initialize(require('../source/shaders/ndcvertices.vert'));
+
+        /* AO Pass */
+
+        const ao_frag = new Shader(context, gl.FRAGMENT_SHADER, 'ssao.frag');
+        ao_frag.initialize(require('./data/ssao/ssao.frag'));
+
+        this._aoProgram = new Program(context, 'AOProgram');
+        this._aoProgram.initialize([ndc_triangle_vert, ao_frag], true);
+        this._aoProgram.link();
+        this._aoProgram.bind();
+
+        this._uDepthTextureLocation = this._aoProgram.uniform('u_depth');
+        this._uNormalTextureLocation = this._aoProgram.uniform('u_normal');
+        this._uAOFrameSizeLocation = this._aoProgram.uniform('u_frameSize');
+
+        /* Blur Pass */
+
+        const blur_frag = new Shader(context, gl.FRAGMENT_SHADER, 'blur.frag');
+        blur_frag.initialize(require('./data/ssao/blur.frag'));
+
+        this._blurProgram = new Program(context, 'BlurProgram');
+        this._blurProgram.initialize([ndc_triangle_vert, blur_frag], true);
+        this._blurProgram.link();
+        this._blurProgram.bind();
+
+        this._uKernelSizeLocation = this._blurProgram.uniform('u_kernelSize');
+        this._uSourceLocation = this._blurProgram.uniform('u_source');
+        this._uAOFrameSizeLocation = this._blurProgram.uniform('u_frameSize');
+
+        /* Composition Pass */
+
+        const composition_frag = new Shader(context, gl.FRAGMENT_SHADER, 'composition.frag');
+        composition_frag.initialize(require('./data/ssao/composition.frag'));
+
+        this._compositionProgram = new Program(context, 'CompositionProgram');
+        this._compositionProgram.initialize([ndc_triangle_vert, composition_frag], true);
+        this._compositionProgram.link();
+        this._compositionProgram.bind();
+
+        this._uColorLocation = this._compositionProgram.uniform('u_color');
+        this._uDepthLocation = this._compositionProgram.uniform('u_depth');
+        this._uAOMapLocation = this._compositionProgram.uniform('u_aoMap');
+        this._uCompositionFrameSizeLocation = this._compositionProgram.uniform('u_frameSize');
 
         /* Create and Configure Scene Forward Pass. */
 
@@ -204,15 +261,16 @@ export class AmbientOcclusionRenderer extends Renderer {
 
         this._forwardPass.program = this._sceneProgram;
         this._forwardPass.updateModelTransform = (matrix: mat4) => {
-            gl.uniformMatrix4fv(this._uModel, false, matrix);
+            gl.uniformMatrix4fv(this._uModelLocation, false, matrix);
         };
         this._forwardPass.updateViewProjectionTransform = (matrix: mat4) => {
-            gl.uniformMatrix4fv(this._uViewProjection, false, matrix);
+            gl.uniformMatrix4fv(this._uViewProjectionLocation, false, matrix);
         };
 
         this._forwardPass.bindUniforms = () => {
-            gl.uniform3fv(this._uEye, this._camera.eye);
+            gl.uniform3fv(this._uEyeLocation, this._camera.eye);
             gl.uniform1i(this._uNormalLocation, 2);
+            gl.uniform2f(this._uSceneFrameSizeLocation, this._frameSize[0], this._frameSize[1]);
         };
 
         this._forwardPass.bindGeometry = (geometry: Geometry) => {
@@ -226,12 +284,6 @@ export class AmbientOcclusionRenderer extends Renderer {
         };
 
         this.loadAsset();
-
-        /* Initialize Ambient Occlusion Pass */
-
-        /* Initialize Blur Pass */
-
-        /* Initialize Composition Pass */
 
         return true;
     }
@@ -286,7 +338,7 @@ export class AmbientOcclusionRenderer extends Renderer {
     protected onPrepare(): void {
         const gl = this._context.gl;
 
-        this._forwardPass.prepare();
+        /* Resize FBOs */
 
         if (this._altered.frameSize) {
             this._sceneFbo.resize(this._frameSize[0], this._frameSize[1]);
@@ -294,12 +346,33 @@ export class AmbientOcclusionRenderer extends Renderer {
             this._blurredAOFbo.resize(this._frameSize[0], this._frameSize[1]);
         }
 
-        /* This? */
-        if (this._altered.canvasSize) {
-            this._sceneProgram.bind();
-            gl.uniform2f(this._sceneProgram.uniform('u_frameSize'), this._frameSize[0], this._frameSize[1]);
-        }
-        /* */
+        /* Prepare Scene Render Pass */
+
+        this._forwardPass.prepare();
+
+        /* Prepare AO Map Pass */
+
+        this._aoProgram.bind();
+        gl.uniform2f(this._uAOFrameSizeLocation, this._frameSize[0], this._frameSize[1]);
+        gl.uniform1i(this._uNormalTextureLocation, 0);
+        gl.uniform1i(this._uDepthTextureLocation, 1);
+
+        /* Prepare Blur Pass */
+
+        this._blurProgram.bind();
+        gl.uniform2i(this._uKernelSizeLocation, 3, 3);
+        gl.uniform1i(this._uSourceLocation, 0);
+        gl.uniform2f(this._uBlurFrameSizeLocation, this._frameSize[0], this._frameSize[1]);
+
+        /* Prepare Composition Pass */
+
+        this._compositionProgram.bind();
+        gl.uniform1i(this._uColorLocation, 0);
+        gl.uniform1i(this._uDepthLocation, 1);
+        gl.uniform1i(this._uAOMapLocation, 2);
+        gl.uniform2f(this._uCompositionFrameSizeLocation, this._frameSize[0], this._frameSize[1]);
+
+        /* Remainder */
 
         this._altered.reset();
         this._camera.altered = false;
@@ -312,35 +385,43 @@ export class AmbientOcclusionRenderer extends Renderer {
             return;
         }
 
+        /* Scene Pass */
+
         this._sceneFbo.bind();
         this._sceneFbo.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT, false, false);
 
         gl.drawBuffers([gl.COLOR_ATTACHMENT0, gl.COLOR_ATTACHMENT0 + 1, gl.COLOR_ATTACHMENT0 + 2]);
 
+        this._sceneProgram.bind();
+
         this._forwardPass.frame();
 
+        /* Ambient Occlusion Pass */
+
         this._aoFbo.bind();
         this._aoFbo.clear(gl.COLOR_BUFFER_BIT, false, false);
 
+        this._sceneNormal.bind(gl.TEXTURE0);
+        this._sceneLinearDepth.bind(gl.TEXTURE1);
+
         gl.drawBuffers([gl.COLOR_ATTACHMENT0]);
+
+        this._aoProgram.bind();
 
         this._ndcGeometry.bind();
         this._ndcGeometry.draw();
         this._ndcGeometry.unbind();
 
-        this._aoFbo.bind();
-        this._aoFbo.clear(gl.COLOR_BUFFER_BIT, false, false);
+        /* Blur Pass */
+
+        this._blurredAOFbo.bind();
+        this._blurredAOFbo.clear(gl.COLOR_BUFFER_BIT, false, false);
+
+        this._aoNoisyMap.bind(gl.TEXTURE0);
 
         gl.drawBuffers([gl.COLOR_ATTACHMENT0]);
 
-        this._ndcGeometry.bind();
-        this._ndcGeometry.draw();
-        this._ndcGeometry.unbind();
-
-        this._outputFbo.bind();
-        this._outputFbo.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT, false, false);
-
-        gl.drawBuffers([gl.COLOR_ATTACHMENT0]);
+        this._blurProgram.bind();
 
         this._ndcGeometry.bind();
         this._ndcGeometry.draw();
@@ -348,6 +429,28 @@ export class AmbientOcclusionRenderer extends Renderer {
     }
 
     protected onSwap(): void {
+        const gl = this._context.gl;
+
+        if (this.isLoading) {
+            return;
+        }
+
+        /* Composition Pass */
+
+        this._outputFbo.bind();
+        this._outputFbo.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT, false, false);
+
+        this._sceneColor.bind(gl.TEXTURE0);
+        this._sceneLinearDepth.bind(gl.TEXTURE0);
+        this._aoMap.bind(gl.TEXTURE0);
+
+        this._compositionProgram.bind();
+
+        gl.drawBuffers([gl.BACK]);
+
+        this._ndcGeometry.bind();
+        this._ndcGeometry.draw();
+        this._ndcGeometry.unbind();
     }
 
     /**
